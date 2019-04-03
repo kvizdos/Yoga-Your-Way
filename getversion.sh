91e00c76e8934032de5222d2e20da41540d20369
Kenton Vizdos (MainPC) <kvizdos@gmail.com>
2019-04-03 13:24:05 -0400

    Version tracker

--- getversion.sh
+++ getversion.sh
@@ -2,4 +2,4 @@
 
 VERSION=`git rev-parse --short HEAD`
 echo "Latest: $VERSION";
-echo "const _LASTCOMMIT = '$VERSION';" > version.js
+echo "const _LASTCOMMIT = '$VERSION';console.log('Current Version: ' + _LASTCOMMIT);" > version.js
